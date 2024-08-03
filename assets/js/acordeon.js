fb288bcb2918429b1e3e339162083ce13fed8d4a
Luiz Fabio <luizbinho26@gmail.com>
2024-08-03 19:36:12 -0300

    Ad alteracoes no projeto

--- assets/js/acordeon.js
+++ assets/js/acordeon.js
@@ -1,34 +1,34 @@
-// const projectTrig = document.querySelectorAll(".acordeon .trig")
+const projectTrig = document.querySelectorAll(".acordeon .trig")
 
-// projectTrig.forEach((trig) => {
-//     trig.addEventListener('click',(e)=>{
-//         const project = trig.parentElement
-//         const isOpen = project.classList.contains('open')
-
-//         if (isOpen) {
-//             project.classList.remove('open')
-//          } else{
-//             project.classList.add('open')
-//         }
-       
-//    }) 
-
-// })
-   
-
-const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
-
-acordeonTriggers.forEach((trigger) => {
-    trigger.addEventListener('click', (e) => {
-        const acordeon = trigger.parentElement
-        const isOpen = acordeon.classList.contains('open')
+projectTrig.forEach((trig) => {
+    trig.addEventListener('click',(e)=>{
+        const project = trig.parentElement
+        const isOpen = project.classList.contains('open')
 
         if (isOpen) {
-            acordeon.classList.remove('open')
-        } else {
-            acordeon.classList.add('open')
+            project.classList.remove('open')
+         } else{
+            project.classList.add('open')
         }
-    })
+       
+   }) 
+
 })
+   
+
+// const acordeonTriggers = document.querySelectorAll('.acordeon .trigger')
+
+// acordeonTriggers.forEach((trigger) => {
+//     trigger.addEventListener('click', (e) => {
+//         const acordeon = trigger.parentElement
+//         const isOpen = acordeon.classList.contains('open')
+
+//         if (isOpen) {
+//             acordeon.classList.remove('open')
+//         } else {
+//             acordeon.classList.add('open')
+//         }
+//     })
+// })
 
 
