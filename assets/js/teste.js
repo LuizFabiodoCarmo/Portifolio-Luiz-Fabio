c9029c727d305d5891a23e8107089ea7837c5062
Luiz Fabio <luizbinho26@gmail.com>
2024-08-05 13:00:42 -0300

    Ad alteracoes no projeto

---
+++ assets/js/teste.js
@@ -0,0 +1,16 @@
+const projectTrig = document.querySelectorAll(".acordeon .trig")
+
+projectTrig.forEach((trig) => {
+    trig.addEventListener('click',(e)=>{
+        const project = trig.parentElement
+        const isOpen = project.classList.contains('open')
+
+        if (isOpen) {
+            project.classList.remove('open')
+         } else{
+            project.classList.add('open')
+        }
+       
+   }) 
+
+})
